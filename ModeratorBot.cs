f642589f965c3d83b42c756f4e551cb0367c9bd4
Ivan Gechev <ivan_banov@abv.bg>
2022-03-23 15:11:23 +0200

    Added Profanity warning and more greeting scenarious

--- ModeratorBot.cs
+++ ModeratorBot.cs
@@ -6,6 +6,18 @@ namespace TheModeratorBot
     public class ModeratorBot
     {
         private HashSet<string> _UsersGreeted = new HashSet<string>();
+        private HashSet<string> _UsersWarned = new HashSet<string>();
+
+        private List<string> _ProfanityWords = new List<string>()
+        {
+            "fuck",
+            "cunt",
+            "asshole",
+            "bitch",
+            "dickhead",
+            "dick"
+        };
+
         public void Initialize(DiscordClient client)
         {
             client.MessageCreated += OnMessageCreated;
@@ -13,7 +25,14 @@ namespace TheModeratorBot
 
         private async Task OnMessageCreated(DiscordClient client, MessageCreateEventArgs messageArgs)
         {
-            if (messageArgs.Message.Content.StartsWith("Hello"))
+            if (messageArgs.Author.Username == "TheModeratorBot")
+            {
+                return;
+            }
+
+            string message = messageArgs.Message.Content.ToLower();
+
+            if (message.StartsWith("hello") || message.StartsWith("hi") || message.StartsWith("hey"))
             {
                 if (_UsersGreeted.Contains(messageArgs.Author.Username))
                 {
@@ -21,8 +40,15 @@ namespace TheModeratorBot
                 }
                 else
                 {
+                    _UsersGreeted.Add(messageArgs.Author.Username);
+
                     await client.SendMessageAsync(messageArgs.Channel, $"Hello, {messageArgs.Author.Username}"); 
                 }
+            }
+            else if (_ProfanityWords.Any(x=>message.Contains(x)))
+            {
+                // TODO: Kick repeating offenders
+                await client.SendMessageAsync(messageArgs.Channel, $"We don't allow bad language here {messageArgs.Author.Username}, play nice or you will be kicked out");
             }
         }
     }
