03e765750302be4561440a2622d2790a559d03fb
Ivan Gechev <ivan_banov@abv.bg>
2022-03-23 12:39:27 +0200

    Added DSharpPlus and basic structure

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using DSharpPlus;
+
+var source = new CancellationTokenSource();
+
+var discordClient = new DiscordClient(new DiscordConfiguration
+{
+    Token = "TOKEN",
+    TokenType = TokenType.Bot
+});
+
+discordClient.MessageCreated += async (client, messageArgs) =>
+{
+    if (messageArgs.Message.Content.StartsWith("Hello"))
+    {
+        await discordClient.SendMessageAsync(messageArgs.Channel, $"Hello, {messageArgs.Author.Username}");
+    }
+};
+
+var token = source.Token;
+
+await discordClient.ConnectAsync();
+
+while (!token.IsCancellationRequested)
+{
+    await Task.Delay(100);
+}
